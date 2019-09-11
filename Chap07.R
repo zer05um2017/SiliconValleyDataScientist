1236c2b0c3e8fbc508d114523f075526083f9d7b
zer05um2017 <perry9122@gmail.com>
2019-09-11 14:47:00 +0900

    Update Chap07.R

--- Chap07.R
+++ Chap07.R
@@ -49,3 +49,34 @@ c(mean(hwy), sd(hwy))
 c(median(hwy), mad(hwy))
 
 # 7.4
+set.seed(1606)
+n <- 100
+p <- 0.5
+x <- rbinom(n, 1, p)
+x <- factor(x, levels = c(0,1), labels = c("no", "yes"))
+x
+table(x)
+prop.table(table(x))
+barplot(table(x))
+binom.test(x=length(x[x=='yes']), n = length(x), p = 0.5, alternative = "two.sided")
+
+# 7.4.1
+binom.test(x=5400, n = 10000)
+
+n <- c(100, 1000, 2000, 10000, 1e6)
+data.frame(n=n, moe=round(1.96 * sqrt(1/(4 * n)), 4))
+curve(1.96 * sqrt(1/(4 * x)), 10, 10000, log='x')
+grid()
+
+# 7.6
+ggplot(mpg, aes(cty, hwy)) + 
+  geom_jitter() + 
+  geom_smooth(method="lm")
+
+cor(mpg$cty, mpg$hwy)
+with(mpg, cor(cty, hwy))
+with(mpg, cor(cty, hwy, method = "kendall"))
+with(mpg, cor(cty, hwy, method = "spearman"))
+
+(hwy_lm <- lm(hwy ~ cty, data=mpg))
+summary(hwy_lm)
